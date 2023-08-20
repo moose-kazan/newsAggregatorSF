40f70865b0a1c75f38c9694345ed6a593ad0e9db
Vadim Kalinnikov <moose@ylsoftware.com>
2023-08-20 16:47:40 +0300

    Make database persistent

--- serviceComments/run.sh
+++ serviceComments/run.sh
@@ -4,6 +4,8 @@ cd `dirname $0`
 
 while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 1; done
 
-psql postgresql://postgres:postgres@${DB_HOST}:${DB_PORT}/multirss < ./db_schema.sql
+test ! -f /lock/comments-schema.lock && \
+    psql postgresql://postgres:postgres@${DB_HOST}:${DB_PORT}/multirss < ./db_schema.sql && \
+    touch /lock/comments-schema.lock
 
 ./serviceComments
--- serviceNews/run.sh
+++ serviceNews/run.sh
@@ -4,7 +4,12 @@ cd `dirname $0`
 
 while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 1; done
 
-psql postgresql://postgres:postgres@${DB_HOST}:${DB_PORT}/multirss < ./db_schema.sql
-psql postgresql://postgres:postgres@${DB_HOST}:${DB_PORT}/multirss < ./db_data.sql
+test ! -f /lock/news-schema.lock && \
+    psql postgresql://postgres:postgres@${DB_HOST}:${DB_PORT}/multirss < ./db_schema.sql && \
+    touch /lock/news-schema.lock
+
+test ! -f /lock/news-data.lock && \
+    psql postgresql://postgres:postgres@${DB_HOST}:${DB_PORT}/multirss < ./db_data.sql && \
+    touch /lock/news-data.lock
 
 ./serviceNews
